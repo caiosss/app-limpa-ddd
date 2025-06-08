bf4002ae49afea3531c08cb6935a89c78a47ba07
Caio <caiobarros1712@gmail.com>
2025-06-08 12:10:39 -0300

    feat: refactor Entidade and Aula models to use props interface and improve structure

--- packages/educacional/src/curso/models/Aula.ts
+++ packages/educacional/src/curso/models/Aula.ts
@@ -1,9 +1,33 @@
-import { Entidade } from "shared";
+import { Entidade, EntidadeProps } from "shared";
 
-export interface Aula extends Entidade {
-    nome: string
-    duracao: number
-    videoURL: string
-    visivel: boolean
-    ordem: number
+export interface AulaProps extends EntidadeProps {
+    nome?: string
+    duracao?: number
+    videoURL?: string
+    visivel?: boolean
+    ordem?: number
+}
+
+export default class Aula extends Entidade<Aula, AulaProps> {
+    constructor(readonly props: AulaProps) {
+        super(props)
+    }
+
+    static novo(props: Partial<AulaProps>): Aula {
+        return new Aula({
+            ...props
+        } as AulaProps)
+    }
+    
+    get nome() { 
+        return this.props.nome 
+    }
+
+    get duracao() {
+        return this.props.duracao
+    }
+
+    get video() {
+        return this.props.videoURL
+    }
 }
--- packages/shared/src/Entidade.ts
+++ packages/shared/src/Entidade.ts
@@ -1,3 +1,35 @@
-export interface Entidade {
-    id?: string
+import Id from "./Id"
+
+export interface EntidadeProps {
+    id: string
+}
+
+export default class Entidade<E, P extends EntidadeProps> {
+
+    constructor(readonly props: P){
+        this.props = {
+            ...props, 
+            id: props.id ?? Id.newHash(),
+        }
+    }
+
+    get id() {
+        return this.props.id
+    }
+
+    igual(entidade: E):boolean {
+        return this.id === (entidade as any).id
+    }
+
+    diferente(entidade: E): boolean {
+        return !this.igual(entidade)
+    }
+
+    clone(props: Partial<P>): E {
+        return new(this.constructor as any)({
+            ...this.props, 
+            ...props
+        })
+    }
+
 }
